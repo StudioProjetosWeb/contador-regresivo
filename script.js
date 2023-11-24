496b4d55d7b7fbb9f60057cc72515d13c6594f57
DIOGENES GOMES SANTOS DA SILVA <133171059+StudioProjetosWeb@users.noreply.github.com>
2023-11-24 00:08:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+const countdown = document.getElementById('countdown');
+const daysSpan = document.getElementById('days');
+const hoursSpan = document.getElementById('hours');
+const minutesSpan = document.getElementById('minutes');
+const secondsSpan = document.getElementById('seconds');
+const afterCountdownButton = document.getElementById('after-countdown');
+
+const targetDate = new Date('December 8, 2023 00:00:00').getTime();
+
+function updateCountdown() {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+    if (distance < 0) {
+        countdown.style.display = 'none';
+        afterCountdownButton.style.display = 'block';
+        clearInterval(interval);
+        return;
+    }
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    daysSpan.textContent = days;
+    hoursSpan.textContent = hours;
+    minutesSpan.textContent = minutes;
+    secondsSpan.textContent = seconds;
+}
+
+const interval = setInterval(updateCountdown, 1000);
+
+afterCountdownButton.addEventListener('click', function() {
+    window.location.href = 'https://www.externalsite.com';
+});
 
